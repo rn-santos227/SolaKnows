b49859b624a6786ad9b22b603bb896b13500309e
rn-santos227 <rn_santos227@yahoo.com>
2024-04-09 10:37:26 +0800

    update global.css and root apge.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import Head from 'next/head'
 export default function Index() {
   return (
     <>
-      <div className="landing-page-container">
-        <Head>
-          <title>SolaKnows</title>
-        </Head>
-      </div>
+
     </>
   );
 }
