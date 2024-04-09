4f62ade7b5c5ef60c1fbaa283e060793dfac98af
rn-santos227 <rn_santos227@yahoo.com>
2024-04-09 19:56:00 +0800

    update root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Header from "../components/shared/Header"
 
 export default function Index() {
   return (
-    <main className="background flex min-h-screen flex-col items-center justify-between p-24">
+    <>
       <Header />
-    </main>
+      <main className="background flex min-h-screen flex-col items-center justify-between p-24">
+      </main>
+    </>
   );
 }
