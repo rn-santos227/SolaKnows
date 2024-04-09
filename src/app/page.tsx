e49acc41ecda1712bf53c2fb15972cb06a2d0c9b
rn-santos227 <rn_santos227@yahoo.com>
2024-04-09 12:29:21 +0800

    added background in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Index() {
   return (
     <>
-      <div className="background">
-      </div>
+      <main className="background flex min-h-screen flex-col items-center justify-between p-24">
+      </main>
     </>
   );
 }
