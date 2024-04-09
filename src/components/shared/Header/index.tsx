51cfba0e063cf827da9017b3dfb2f28f9cec85f0
rn-santos227 <rn_santos227@yahoo.com>
2024-04-09 18:31:32 +0800

    update header index.tsx

---
+++ src/components/shared/Header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="absolute top-0 left-0 w-full text-white p-4">
+      <h1 className="text-2xl font-bold">SolaKnows</h1>
+    </header>
+  );
+};
+
+export default Header;
