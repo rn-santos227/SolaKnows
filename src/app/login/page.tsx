a78a959993b3759c7794b60401aaf5df71121d32
rn-santos227 <rn_santos227@yahoo.com>
2024-04-09 10:31:19 +0800

    added page.tsx in root

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 export default function Login() {
   return (
-    <h1>login</h1>
-  )
+    <>
+      <h1>login</h1>
+    </>
+  );
 }
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Index() {
+  return (
+    <>
+      <div className="landing-page-container">
+        <Head>
+          <title>SolaKnows</title>
+        </Head>
+      </div>
+    </>
+  );
+}
