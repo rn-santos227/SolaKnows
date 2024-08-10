489dd43f415a2f190a73f9aeca47f81ebec81917
rn-santos227 <rn_santos227@yahoo.com>
2024-08-10 13:04:18 +0800

    updated header component

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -4,7 +4,7 @@ import './style.css';
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full text-white p-4 flex justify-between items-center bg-black shadow-lg z-50">
+    <header className="fixed top-0 left-0 w-full text-white p-1 flex justify-between items-center bg-black shadow-lg z-50">
       <div className="flex items-center">
         <Image src="/images/logo-title.png" alt="Logo" width={200} height={200} />
       </div>
