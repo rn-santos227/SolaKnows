0107e9373498e1c4b515e61956636d5f8f730d5e
rn-santos227 <rn_santos227@yahoo.com>
2024-08-10 15:02:38 +0800

    updated header component

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -20,12 +20,13 @@ const Header = () => {
   };
 
   return (
-    <header className={`fixed top-0 left-0 w-full text-white p-1 flex justify-between items-center bg-black ${isHomePage ? 'shadow-light-green' : 'shadow-lg'} z-50`}>
+    <header className={`fixed top-0 left-0 w-full text-white p-2 flex justify-between items-center bg-black ${isHomePage ? 'shadow-light-green' : 'shadow-lg'} z-50`}>
       <div className="flex items-center">
         <Image src="/images/logo-title.png" alt="Logo" width={200} height={200} />
       </div>
       <div className="flex space-x-4 mx-4">
         <Button text="LOGIN" onClick={handleLogin} className="green-border" />
+        <Button text="SIGN UP" onClick={handleSignup} className="green-filled" />
       </div>
     </header>
   );
