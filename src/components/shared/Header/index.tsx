b75ec43d0a75e2907b0cdfc95ae06735a89b6b19
rn-santos227 <rn_santos227@yahoo.com>
2024-08-10 12:21:38 +0800

    updated header component

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
+
 import './style.css';
 
 const Header = () => {
   return (
-    <header className="absolute top-0 left-0 w-full text-white p-4">
-      <h1 className="text-2xl font-bold">SolaKnows</h1>
+    <header className="fixed top-0 left-0 w-full text-white p-4 flex justify-between items-center bg-black shadow-lg z-50">
+      <div className="flex items-center">
+        <Image src="/images/logo-title.png" alt="Logo" width={250} height={250} />
+      </div>
+      <div className="flex space-x-4">
+
+      </div>
     </header>
   );
 };
