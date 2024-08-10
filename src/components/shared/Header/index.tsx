51f81ed6be6917ffc6304d733ebea6a4fc0e369b
rn-santos227 <rn_santos227@yahoo.com>
2024-08-10 12:29:39 +0800

    created button component

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full text-white p-4 flex justify-between items-center bg-black shadow-lg z-50">
       <div className="flex items-center">
-        <Image src="/images/logo-title.png" alt="Logo" width={250} height={250} />
+        <Image src="/images/logo-title.png" alt="Logo" width={200} height={200} />
       </div>
       <div className="flex space-x-4">
 
