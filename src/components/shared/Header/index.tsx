defd4d520c622c3e6848f724585ed970fa6131da
rn-santos227 <rn_santos227@yahoo.com>
2024-08-10 13:09:55 +0800

    updated header component

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   const isHomePage = pathname === '/';
 
   return (
-    <header className={`fixed top-0 left-0 w-full text-white p-1 flex justify-between items-center bg-black${isHomePage ? 'shadow-light-green' : 'shadow-lg'} z-50`}>
+    <header className={`fixed top-0 left-0 w-full text-white p-1 flex justify-between items-center bg-black ${isHomePage ? 'shadow-light-green' : 'shadow-lg'} z-50`}>
       <div className="flex items-center">
         <Image src="/images/logo-title.png" alt="Logo" width={200} height={200} />
       </div>
