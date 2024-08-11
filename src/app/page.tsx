b8edc4da98480d3d550d82ea551327183b686077
rn-santos227 <rn_santos227@yahoo.com>
2024-08-11 19:39:57 +0800

    updated root page tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { usePathname } from 'next/navigation';
-
 export default function Index() {
-  const pathname = usePathname();
-  const isHomePage = pathname === '/';
 
   return (
     <>
-      <header className={`fixed top-0 left-0 w-full text-white p-2 flex justify-between items-center bg-black ${isHomePage ? 'shadow-light-green' : 'shadow-lg'} z-50`}>
-        
+      <header className="fixed top-0 left-0 w-full text-white p-2 flex justify-between items-center bg-black shadow-light-green z-50">
+
       </header>
       <main className="background flex min-h-screen flex-col items-center justify-between p-24">
       </main>
