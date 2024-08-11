ca6e4d0fd888ef0311ff1cf1bcce786527695dee
rn-santos227 <rn_santos227@yahoo.com>
2024-08-11 19:38:15 +0800

    updated root page tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 import { usePathname } from 'next/navigation';
 
 export default function Index() {
+  const pathname = usePathname();
+  const isHomePage = pathname === '/';
+  
   return (
     <>
       <main className="background flex min-h-screen flex-col items-center justify-between p-24">
