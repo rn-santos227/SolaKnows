b444992f014152a3d83bf26e8281964cb62dc360
rn-santos227 <rn_santos227@yahoo.com>
2024-08-11 19:39:54 +0800

    updated root page tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ export default function Index() {
 
   return (
     <>
+      <header className={`fixed top-0 left-0 w-full text-white p-2 flex justify-between items-center bg-black ${isHomePage ? 'shadow-light-green' : 'shadow-lg'} z-50`}>
+        
+      </header>
       <main className="background flex min-h-screen flex-col items-center justify-between p-24">
       </main>
     </>
