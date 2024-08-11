afdd7ff6986b886a2933f1fe4c456325a748d6dc
rn-santos227 <rn_santos227@yahoo.com>
2024-08-11 19:46:10 +0800

    updated root page tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 
 export default function Index() {
@@ -17,7 +17,10 @@ export default function Index() {
   return (
     <>
       <header className="fixed top-0 left-0 w-full text-white p-2 flex justify-between items-center bg-black shadow-light-green z-50">
-
+        <div className="flex items-center">
+          <Image src="/images/logo-title.png" alt="Logo" width={200} height={200} />
+        </div>
+        <div className="flex space-x-4 mx-4"></div>
       </header>
       <main className="background flex min-h-screen flex-col items-center justify-between p-24">
       </main>
