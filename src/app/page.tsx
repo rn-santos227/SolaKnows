12af648e9b2f1635fe653378ac33578af30387af
rn-santos227 <rn_santos227@yahoo.com>
2024-08-11 19:37:53 +0800

    updated root page tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Header from "../components/shared/Header"
+
+import { usePathname } from 'next/navigation';
 
 export default function Index() {
   return (
     <>
-      <Header />
       <main className="background flex min-h-screen flex-col items-center justify-between p-24">
       </main>
     </>
